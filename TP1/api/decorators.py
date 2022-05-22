746b18d9fcaa41a0d94e8736e8e729781c0bfcb9
Luiz Facury <luizfysouza@gmail.com>
2022-05-22 13:15:45 -0300

    Alterações de autenticação 2

--- TP1/api/decorators.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-def unauthorized_user(view_func):
-    def wrapper_func (request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('usuario:Perfil')
-        else:
-            return view_func(request, *args, **kwargs)
-    return wrapper_func
